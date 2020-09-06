6a417c3efb28a54a73e9de3bf6e3ad1ce44bf38a
kleyson castro <kleysoncastro99@gmail.com>
2020-09-06 14:40:52 -0400

    styles: compoente com eslitos global aplicação

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,12 +9,18 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #F0F2F5 ;
+    background: #312e38;
+    color: #fff;
     -webkit-font-smoothing: antialiased
   }
 
   body, input, button {
-    font: 16px "Poppins", sans-serif;
+    font-family: "Roboto Slab", serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
   }
 
   button {
